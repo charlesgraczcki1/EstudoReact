f7d2d78e78765d8855d9b700fa35bacf47784b71
charlesgraczcki <charlesgraczcki@gmail.com>
2025-12-28 12:23:32 -0300

    finalizado review do js

--- apendice-js/js/scripts.js
+++ apendice-js/js/scripts.js
@@ -74,3 +74,61 @@ products.map((product) => {
   if (product.category !== "Roupas") product.onSale = true;
 });
 console.log(products);
+
+//template literals
+const userName = "Charles";
+const age = 28;
+
+console.log(`O nome do usuário é ${userName} e ele tem ${age} anos.`);
+
+//destructuring
+const fruits = ["Maçã", "Banana", "Morango"];
+const [i1, i2, i3] = fruits;
+console.log(i1, i2, i3);
+const productDetails = { name: "Tênis", price: 89.99, category: "Calçados" };
+const { name: productName, price, category } = productDetails;
+console.log(productName, price, category);
+//spread operator
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+const arr3 = [...arr1, ...arr2];
+console.log(arr3);
+const car = { brand: "Ford", model: "Ka", year: 2020 };
+const carClone = { ...car, color: "red" };
+console.log(carClone);
+
+//classes
+class Product {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+
+  productWithDiscount(discount) {
+    return (this.price * (100 - discount)) / 100;
+  }
+}
+const shirt = new Product("Camisa", 20);
+console.log(shirt.name);
+console.log(shirt.price);
+console.log(shirt.productWithDiscount(20));
+console.log(shirt.productWithDiscount(50));
+
+//herança
+class SuperProduct extends Product {
+  constructor(name, price, colors) {
+    super(name, price);
+    this.colors = colors;
+  }
+  showColors() {
+    console.log("As cores disponíveis são:");
+    this.colors.forEach((color) => {
+      console.log(color);
+    });
+  }
+}
+const tenis = new SuperProduct("Tênis", 120, ["preto", "branco", "azul"]);
+console.log(tenis.name);
+console.log(tenis.price);
+console.log(tenis.productWithDiscount(30));
+tenis.showColors();
